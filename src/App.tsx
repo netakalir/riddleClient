cf793e3c3235255baaf9a79f8d5819371935d0fe
Netanel kalir <netanelkalir@gmail.com>
2025-09-02 15:19:17 +0300

    pages added

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router";
+
+export default function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={}/>
+      </Routes>
+    </div>
+  )
+}
---
+++ src/pages/LeaderBord.tsx
@@ -0,0 +1,6 @@
+
+export default function LeaderBord() {
+  return (
+    <div>LeaderBord</div>
+  )
+}
